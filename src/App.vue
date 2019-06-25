9900699e1335bc1e61cc91a721f9419f4f412e74
hobbs <lizhongze.chn@gmail.com>
2019-06-25 17:23:42 +0800

    增加注释

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
+<!-- template：模版标签，相当于 react 中的 render 标签返回的jsx标签 -->
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <div id="app">
+        <!-- <img src="./assets/logo.png"> -->
+        <!-- 路由试图容器，相当于react中的 <Route path='/' component={}/> -->
+        <!-- <router-view/> -->
+        <MyVue/>
+        <!-- 3.调用当前组件 -->
+    </div>
 </template>
 
+<!-- 当前的模版的js代码 -->
 <script>
+import MyVue from "./components/MyVue"; // 1.导入组件
 export default {
-  name: 'App'
-}
+    // 2.注册组件到当前 App.vue 中
+    name: "App",
+    components: { MyVue }
+};
 </script>
 
+<!-- 当前的模版的css代码 -->
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue' // 引用 vue 框架
+import App from './App' // 引用 App.vue 根组件
+import router from './router' // 引用路由文件
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+  el: '#app', // 表示当前实例在 index.html 文件中 id="app" 的标签
+  router, // 表示当前 vue实例的路由配置文件
+  components: { App }, // 表示当前实例可以使用的 组件
+  template: '<App/>' // 表示当前实例使用的模版为 App.vue
 })
