671cd1d8115bcba262f4538c8e312ad958f5e71a
hobbs <lizhongze.chn@gmail.com>
2019-06-26 18:17:18 +0800

    父组件传数据给子组件练习

---
+++ src/components/MyProps.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="myProps">
+		<p>my props</p>
+		{{a}}----{{b}}----{{c}}
+		{{rePorp()}}
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'MyProps',
+	props: ['a', 'b', 'c'],
+	methods: {
+		rePorp() {
+			this.$emit('abc', '123')
+		}
+	}
+}
+</script>
+
