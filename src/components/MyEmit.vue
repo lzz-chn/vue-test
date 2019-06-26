37b24da58a3503cd1f750bd5ed2ae3b6e7e80e6c
hobbs <lizhongze.chn@gmail.com>
2019-06-26 14:48:26 +0800

    $emit() 方法练习

---
+++ src/components/MyEmit.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="myEmit">
+		<span>子组件：</span>
+		<input type="text" v-on:keyup="$emit('myEmit',dio)" v-model="dio">
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return { dio: '' }
+	}
+}
+</script>
+
