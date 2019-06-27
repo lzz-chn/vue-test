0c541cfd72a22eeff1307ff12e8d83716cc62c08
hobbs <lizhongze.chn@gmail.com>
2019-06-27 18:10:56 +0800

    增加继承练习

---
+++ src/components/MyExtends.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="myExtends">
+		<h3>My Extends</h3>
+		{{jojo}}
+		{{yo()}}
+		<!-- 全局混入 -->
+		{{mixin_ajax()}}
+		{{extend_ajax()}}
+	</div>
+</template>
+
+<script>
+let my_extends = {
+	methods: {
+		yo() {
+			console.log('Yo.')
+		}
+	},
+	data() {
+		return {
+			jojo: 'olaolaola'
+		}
+	}
+}
+export default {
+	name: 'MyExtends',
+	extends: my_extends
+}
+</script>
