f3a9bac1e0305bdc9762acec7bc7189ecb0032ea
hobbs <lizhongze.chn@gmail.com>
2019-06-27 16:17:13 +0800

    Provide and Inject 数据传递练习

---
+++ src/components/ProvideInject.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="provideInject">
+		<h3>Provide and Inject</h3>
+		<div>父级组件数据 jojo: {{jojo}}</div>
+		<div>祖父级组件数据 dio: {{dio}}</div>
+        <div>provide 和 inject 绑定并不是可响应的 inp: {{inp}}</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'ProvideInject',
+	props: ['jojo'],
+    inject: ['dio','inp'],
+}
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue' // 引用 vue 框架
-import App from './App' // 引用 App.vue 根组件
-import router from './router' // 引用路由文件
+import Vue from 'vue'; // 引用 vue 框架
+import App from './App'; // 引用 App.vue 根组件
+import router from './router'; // 引用路由文件
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app', // 表示当前实例在 index.html 文件中 id="app" 的标签
-  router, // 表示当前 vue实例的路由配置文件
-  components: { App }, // 表示当前实例可以使用的 组件
-  template: '<App/>' // 表示当前实例使用的模版为 App.vue
-})
+    el: '#app', // 表示当前实例在 index.html 文件中 id="app" 的标签
+    router, // 表示当前 vue实例的路由配置文件
+    components: { App }, // 表示当前实例可以使用的 组件
+    data: { dio: 'mudamudamudamuda', jojo: 'olaolaolaola' },
+    provide() {
+        return { dio: 'mudamudamuda' };
+    },
+    template: '<App/>' // 表示当前实例使用的模版为 App.vue
+});
