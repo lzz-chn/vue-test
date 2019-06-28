0a39fc7c9ba8e5ae5c5951a1aa482f7216ea4426
hobbs <lizhongze.chn@gmail.com>
2019-06-28 15:21:33 +0800

    增加过滤器 filter 练习

---
+++ src/components/MyFilter.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="myFilter">
+		<h3>my filter</h3>
+		<div>日期：{{new Date() | dateFilter}}</div>
+		<div>时间：{{new Date() | timeFilter}}</div>
+		<div>随机数：{{Math.random() | randomFilter}}</div>
+		<input type="text" v-bind:numbers="Math.random() | randomFilter">
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'MyFilter',
+	filters: {
+		dateFilter(val) {
+			return val.toLocaleDateString()
+		},
+		timeFilter(val) {
+			return val.toLocaleTimeString()
+		},
+		randomFilter(num) {
+			return num.toFixed(4)
+		}
+	}
+}
+</script>
